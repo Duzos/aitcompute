3baa3003fd71ac0af494bc2f0b7e4aeada0afdfd
James Hall <james.hall01@outlook.com>
2024-06-23 20:10:53 +0100

    Initial Commit

---
+++ src/main/java/mc/duzo/aitcompute/Computed.java
@@ -0,0 +1,18 @@
+package mc.duzo.aitcompute;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Computed implements ModInitializer {
+	public static final String MOD_ID = "aitcompute";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	/**
+	 * Runs the mod initializer.
+	 */
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/mc/duzo/aitcompute/client/ComputedClient.java
@@ -0,0 +1,13 @@
+package mc.duzo.aitcompute.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ComputedClient implements ClientModInitializer {
+	/**
+	 * Runs the mod initializer on the client environment.
+	 */
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
